f80420835d32c0e48bfc75ace411df579b6f3648
RafalMarczuk <rafmar34@gmail.com>
2023-03-29 10:01:12 +0200

    Initial commit

---
+++ mail.py
@@ -0,0 +1,13 @@
+class EmailExtractor:
+
+    def __init__(self, email: str):
+        self.email = email
+
+    def is_student(self) -> bool:
+        return False
+
+    def get_name(self):
+        pass
+
+    def get_surname(self):
+        pass
